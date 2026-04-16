f8c0a0ea74b648039214a16bb5c3dbc1c6b8f2b9
unknown <gabrielleite03@gmail.com>
2026-04-16 14:07:56 -0300

    fixing workflows

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: 'http://kenjix-alb-1941285083.us-east-1.elb.amazonaws.com:7010',
+  API_URL: 'http://kenjix-alb-1941285083.us-east-1.elb.amazonaws.com/api',
   S3_BUCKET: 'aws-s3-site-kejipet'
 };
